a197118ece5ef23c1f7c51afe9390e036afaeab2
Ziaullah Khan <ziaullahkhan.se@gmail.com>
2026-08-24 21:40:51 +0500

    Update vowel_count.py

--- vowel_count.py
+++ vowel_count.py
@@ -8,9 +8,9 @@ def get_count(sentence):
     
     return count
 
-# Test
-print(get_count("hello"))        # 2
-print(get_count("aeiou"))        # 5
-print(get_count("abcde"))        # 2
-print(get_count("my name"))      # 2
-print(get_count("python"))       # 1
+
+print(get_count("hello"))        
+print(get_count("aeiou"))        
+print(get_count("abcde"))        
+print(get_count("my name"))      
+print(get_count("python"))       
